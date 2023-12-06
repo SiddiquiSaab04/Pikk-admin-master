f5d934970cc00dd7a61bc7c49f960a61f9ccbbbd
shazzi7 <imtiaz@teams.pk>
2023-12-06 16:36:42 +0500

    Dashboard template added.

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ $title ?? 'Pikk Dashboard' }}</title>
+
+    <link href="{{ asset('vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendors/nprogress/nprogress.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendors/bootstrap-daterangepicker/daterangepicker.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('build/css/custom.min.css') }}" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+</head>
+
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+            <div class="col-md-3 left_col">
+                <div class="left_col scroll-view">
+                    <div class="navbar nav_title" style="border: 0;">
+                        <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+                    </div>
+                    <div class="clearfix"></div>
+                    @include('admin.layouts.sidebar')
+                </div>
+            </div>
+            @include('admin.layouts.top_navigation')
+            @yield('content')
+            @include('admin.layouts.footer')
+        </div>
+    </div>
+
+    <script src="{{ asset('vendors/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendors/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendors/fastclick/lib/fastclick.js') }}"></script>
+    <script src="{{ asset('vendors/nprogress/nprogress.js') }}"></script>
+    <script src="{{ asset('vendors/Chart.js/dist/Chart.min.js') }}"></script>
+    <script src="{{ asset('vendors/jquery-sparkline/dist/jquery.sparkline.min.js') }}"></script>
+    <script src="{{ asset('vendors/raphael/raphael.min.js') }}"></script>
+    <script src="{{ asset('vendors/morris.js/morris.min.js') }}"></script>
+    <script src="{{ asset('vendors/gauge.js/dist/gauge.min.js') }}"></script>
+    <script src="{{ asset('vendors/bootstrap-progressbar/bootstrap-progressbar.min.js') }}"></script>
+    <script src="{{ asset('vendors/skycons/skycons.js') }}"></script>
+    <script src="{{ asset('vendors/Flot/jquery.flot.js') }}"></script>
+    <script src="{{ asset('vendors/Flot/jquery.flot.pie.js') }}"></script>
+    <script src="{{ asset('vendors/Flot/jquery.flot.time.js') }}"></script>
+    <script src="{{ asset('vendors/Flot/jquery.flot.stack.js') }}"></script>
+    <script src="{{ asset('vendors/Flot/jquery.flot.resize.js') }}"></script>
+    <script src="{{ asset('vendors/flot.orderbars/js/jquery.flot.orderBars.js') }}"></script>
+    <script src="{{ asset('vendors/flot-spline/js/jquery.flot.spline.min.js') }}"></script>
+    <script src="{{ asset('vendors/flot.curvedlines/curvedLines.js') }}"></script>
+    <script src="{{ asset('vendors/DateJS/build/date.js') }}"></script>
+    <script src="{{ asset('vendors/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('vendors/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+
+    <script src="{{ asset('build/js/custom.min.js') }}"></script>
+</body>
+
+</html>
