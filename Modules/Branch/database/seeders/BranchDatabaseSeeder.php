b76d31aef1e35baaf84410850408a7aeb863656c
haxxan21 <malikhassan053@gmail.com>
2023-12-15 17:53:54 +0500

    Branch module aded

---
+++ Modules/Branch/database/seeders/BranchDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Branch\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class BranchDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
