f7412f368d99a0db608143c3f3e4b730df842fc9
haxxan21 <malikhassan053@gmail.com>
2023-12-15 18:07:40 +0500

    product Controller commented

--- Modules/Inventory/app/Services/ProductService.php
+++ Modules/Inventory/app/Services/ProductService.php
@@ -31,11 +31,19 @@ class ProductService
         $this->model = "\\Modules\\Inventory\\app\\Models\\Product";
     }
 
+    /**
+     * @param null
+     * @return Category
+     */
     public function getCategories()
     {
         return $this->categories->getAllWithoutPagination();
     }
 
+    /**
+     * @param null
+     * @return AddonGroup
+     */
     public function getAddons()
     {
         $addons = $this->addons->getAllWithoutPagination();
@@ -43,12 +51,24 @@ class ProductService
         return $addons;
     }
 
+    /**
+     * returns color associated with each product category
+     *
+     * @param null
+     * @return array
+     */
     public function colorAssociation()
     {
         $categories = array_values($this->getCategories()->pluck('id')->toArray());
         return array_combine($categories, ['primary', 'info', 'warning', 'success']);
     }
 
+    /**
+     * Creates a new Product
+     *
+     * @param array $data
+     * @return Product
+     */
     public function createProduct($data)
     {
         $data['product_addon'] = collect(json_decode($data['addons']), true);
@@ -57,6 +77,10 @@ class ProductService
         $addons = collect($data['product_addon']);
 
         foreach($addons as $addonData) {
+            /**
+             * creating array for product_modifiers table
+             */
+
             $addon = [
                 "modifier_id" => $addonData->id,
                 "product_id" => $product->id,
@@ -65,9 +89,18 @@ class ProductService
 
             $productModifier = $this->modifierService->create($addon);
 
+            /**
+             * we are only selecting the products where is_selected
+             * field is 1, which means only selected products will
+             * be inserted into the database
+             */
             $products = collect($addonData->products)->filter( fn ($product) => $product->is_selected == 1 )->flatten();
 
             foreach ($products as $productData) {
+
+                /**
+                 * creating product_modifier_addons array for insertion
+                 */
                 $addonProduct = [
                     "product_modifier_id" => $productModifier->id,
                     'product_id' => $productData->id
@@ -80,6 +113,14 @@ class ProductService
         return $product;
     }
 
+    /**
+     * Update an existing product
+     *
+     * @param array data
+     * @param string id
+     *
+     * @return Product
+     */
     public function updateProduct($data, $id)
     {
         $product = $this->getById($id);
@@ -88,15 +129,32 @@ class ProductService
 
         $update = $this->update($data, $id);
 
+        /**
+         * fetch a fresh instance of product from database
+         */
         $product = $this->refresh($product);
 
+        /**
+         * removing all exisiting record for the product modifers and
+         * addons, we will insert a new row for each addon
+         */
         $product->addons()->delete();
         $product->addonProducts()->delete();
 
         $addons = collect($data['product_addon']);
 
         foreach($addons as $addonData) {
+            /**
+             * checking if new addon is added for the existing product
+             * if added then process data to insert into the database.
+             * otherwise checking whether user has changed the products
+             * for a specific addon
+             *
+             */
             if(empty($addonData->product_id)) {
+                /**
+                 * aligning data for eloquent update
+                 */
                 $addon = [
                     "modifier_id" => $addonData->id,
                     "product_id" => $product->id,
@@ -107,6 +165,9 @@ class ProductService
                 $products = collect($addonData->products)->filter( fn ($product) => $product->is_selected == 1 )->flatten();
 
                 foreach ($products as $productData) {
+                    /**
+                     * aligning data for eloquent update
+                     */
                     $addonProduct = [
                         "product_modifier_id" => $productModifier->id,
                         'product_id' => $productData->id
