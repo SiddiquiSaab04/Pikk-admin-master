5a86eace84fea0c735bcb6311c700c913bc2040e
shazzi7 <imtiaz@teams.pk>
2023-12-23 23:49:11 +0500

    Api response changes.

--- Modules/Inventory/routes/api.php
+++ Modules/Inventory/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Modules\Inventory\app\Http\Controllers\CategoryController;
 use Modules\Inventory\app\Http\Controllers\ProductController;
 
 /*
@@ -15,6 +16,13 @@ use Modules\Inventory\app\Http\Controllers\ProductController;
     |
 */
 
-Route::prefix('v1')->name('api.')->group(function () {
-    Route::get('/product/index', [ProductController::class, 'index']);
+// Route::prefix('v1')->name('api.')->group(function () {
+//     Route::get('/product/index', [ProductController::class, 'index']);
+// });
+
+Route::prefix('')->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('product/index', [ProductController::class, 'index']);
+        Route::get('category/index', [CategoryController::class, 'index']);
+    });
 });
