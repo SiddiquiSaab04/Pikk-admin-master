90e97b1bf78c7f789f9dac0606ad1ca09689e450
haxxan21 <malikhassan053@gmail.com>
2023-12-29 02:21:14 +0500

    socket integrated

---
+++ Modules/Order/app/Events/OrderCancelledEvent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Order\app\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class OrderCancelledEvent implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    public $order;
+    public $branch;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($order, $branch)
+    {
+        $this->order = $order;
+        $this->branch = $branch;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     */
+    public function broadcastOn()
+    {
+        return new Channel('order-cancelled-channel.'.$this->branch);
+    }
+}
