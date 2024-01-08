f35c0d5f39bb3bc962c91adb4ed62010ca024ced
shazzi7 <imtiaz@teams.pk>
2024-01-08 17:00:34 +0500

    Production build.

--- Modules/Customers/routes/api.php
+++ Modules/Customers/routes/api.php
@@ -15,9 +15,9 @@ use Modules\Customers\app\Http\Controllers\CustomersController;
     |
 */
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.')->group(function () {
-    Route::get('customers', fn (Request $request) => $request->user())->name('customers');
-});
+// Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.')->group(function () {
+//     Route::get('customers', fn (Request $request) => $request->user())->name('customers');
+// });
 
 Route::prefix('customers')->group(function () {
     Route::post('loginOrRegister', [CustomersController::class, 'loginOrRegister']);
