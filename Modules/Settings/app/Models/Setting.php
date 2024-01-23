205c14c8f38f128549e6a5a637fc0b4a5fe8add8
shazzi7 <imtiaz@teams.pk>
2024-01-23 19:26:04 +0500

    Settings seeder added.

---
+++ Modules/Settings/app/Models/Setting.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Settings\app\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        "key",
+        "value",
+        "platform"
+    ];
+}
