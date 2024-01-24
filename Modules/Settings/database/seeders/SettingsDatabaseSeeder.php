10d93262c341432b066701bde388060cadc058fd
shazzi7 <imtiaz@teams.pk>
2024-01-24 13:25:31 +0500

    Setting seeder updated.

--- Modules/Settings/database/seeders/SettingsDatabaseSeeder.php
+++ Modules/Settings/database/seeders/SettingsDatabaseSeeder.php
@@ -36,57 +36,76 @@ class SettingsDatabaseSeeder extends Seeder
 
         $settings = [
             [
-                'key' => "web_lg",
-                'platform' => 'web',
-                'value' => $baseUrl . "web_lg.png"
+                "key" => "web_lg",
+                "value" => [
+                    "platform" => "web",
+                    "value" => $baseUrl . "web_lg.png"
+                ]
             ],
             [
-                'key' => "web_md",
-                'platform' => 'web',
-                'value' => $baseUrl . "web_md.png"
+                "key" => "web_md",
+                "value" => [
+                    "platform" => "web",
+                    "value" => $baseUrl . "web_md.png"
+                ]
             ],
             [
-                'key' => "web_sm",
-                'platform' => 'web',
-                'value' => $baseUrl . "web_sm.png"
+                "key" => "web_sm",
+                "value" => [
+                    "platform" => "web",
+                    "value" => $baseUrl . "web_sm.png"
+                ]
             ],
             [
-                'key' => "pos_lg",
-                'platform' => 'pos',
-                'value' => $baseUrl . "pos_lg.png"
+                "key" => "pos_lg",
+                "value" => [
+                    "platform" => "pos",
+                    "value" => $baseUrl . "pos_lg.png"
+                ]
             ],
             [
-                'key' => "pos_md",
-                'platform' => 'pos',
-                'value' => $baseUrl . "pos_md.png"
+                "key" => "pos_md",
+                "value" => [
+                    "platform" => "pos",
+                    "value" => $baseUrl . "pos_md.png"
+                ]
             ],
             [
-                'key' => "pos_sm",
-                'platform' => 'pos',
-                'value' => $baseUrl . "pos_sm.png"
+                "key" => "pos_sm",
+                "value" => [
+                    "platform" => "pos",
+                    "value" => $baseUrl . "pos_sm.png"
+                ]
             ],
             [
-                'key' => "kds_lg",
-                'platform' => 'kds',
-                'value' => $baseUrl . "kds_lg.png"
+                "key" => "kds_lg",
+                "value" => [
+                    "platform" => "kds",
+                    "value" => $baseUrl . "kds_lg.png"
+                ]
             ],
             [
-                'key' => "kds_md",
-                'platform' => 'kds',
-                'value' => $baseUrl . "kds_md.png"
+                "key" => "kds_md",
+                "value" => [
+                    "platform" => "kds",
+                    "value" => $baseUrl . "kds_md.png"
+                ]
             ],
             [
-                'key' => "kds_sm",
-                'platform' => 'kds',
-                'value' => $baseUrl . "kds_sm.png"
+                "key" => "kds_sm",
+                "value" => [
+                    "platform" => "kds",
+                    "value" => $baseUrl . "kds_sm.png"
+                ]
             ],
         ];
 
         try {
-            collect($settings)->each(function ($settings) {
+            collect($settings)->each(function ($setting) {
+                $setting['value'] = json_encode($setting['value']);
                 Setting::updateOrCreate([
-                    'key' => $settings['key']
-                ], $settings);
+                    'key' => $setting['key']
+                ], $setting);
             });
         } catch (Exception $e) {
             Log::info($e->getMessage());
