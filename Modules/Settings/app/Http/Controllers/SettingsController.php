a80739acbb07c7d0b6196b23818226cf1576319e
haxxan21 <malikhassan053@gmail.com>
2024-01-24 20:26:49 +0500

    function types changed

--- Modules/Settings/app/Http/Controllers/SettingsController.php
+++ Modules/Settings/app/Http/Controllers/SettingsController.php
@@ -99,7 +99,7 @@ class SettingsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, $id): RedirectResponse
+    public function update(Request $request, $id)
     {
         //
     }
