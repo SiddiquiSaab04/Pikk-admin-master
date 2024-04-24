d659586e029bf6cd1eb43c7c8f6b4c140e557389
shazzi7 <imtiaz@teams.pk>
2024-04-24 15:11:05 +0500

    Gallery issue fixed.

--- Modules/Inventory/resources/views/products/partials/stocks.blade.php
+++ Modules/Inventory/resources/views/products/partials/stocks.blade.php
@@ -8,8 +8,7 @@
         <p class="mb-0">
             <label for="wholesale_price">Wholesale Price</label>
         </p>
-        <input type="number" class="form-control" name="wholesale_price" id="name"
-            value="{{ $product->wholesale_price ?? 0 }}">
+        <input type="number" class="form-control" name="wholesale_price" id="name" value="{{ $product->wholesale_price ?? 0 }}">
         <span class="mt-1">
             <p class="mt-1">Enter wholesale price for the product</p>
         </span>
@@ -18,8 +17,7 @@
         <p class="mb-0">
             <label for="sale_price">Sale Price</label>
         </p>
-        <input type="number" class="form-control" name="sale_price" id="name"
-            value="{{ $product->sale_price ?? 0 }}">
+        <input type="number" class="form-control" name="sale_price" id="name" value="{{ $product->sale_price ?? 0 }}">
         <span class="mt-1">
             <p class="mt-1">Enter sale price for the product</p>
         </span>
@@ -29,42 +27,36 @@
             <label for="status">Status</label>
         </p>
         <div class="btn-group w-25">
-            <button class="btn btn-success" type="button" value="1"
-                onclick="event.preventDefault(); document.getElementById('status').value = this.value;">Enable</button>
-            <button class="btn btn-danger" type="button" value="0"
-                onclick="event.preventDefault(); document.getElementById('status').value = this.value;">Disable</button>
+            <button class="btn btn-success" type="button" value="1" onclick="event.preventDefault(); document.getElementById('status').value = this.value;">Enable</button>
+            <button class="btn btn-danger" type="button" value="0" onclick="event.preventDefault(); document.getElementById('status').value = this.value;">Disable</button>
         </div>
         <span>
             <p class="mt-1">toggle product status to enable or disable </p>
         </span>
     </div>
-</div>
-<div class="col-sm-4">
-    <p class="mb-0">
-        <label for="status">New</label>
-    </p>
-    <div class="btn-group w-25">
-        <button class="btn btn-success" type="button" value="1"
-            onclick="event.preventDefault(); document.getElementById('is_new').value = this.value;">True</button>
-        <button class="btn btn-danger" type="button" value="0"
-            onclick="event.preventDefault(); document.getElementById('is_new').value = this.value;">False</button>
+
+    <div class="col-sm-4">
+        <p class="mb-0">
+            <label for="status">New</label>
+        </p>
+        <div class="btn-group w-25">
+            <button class="btn btn-success" type="button" value="1" onclick="event.preventDefault(); document.getElementById('is_new').value = this.value;">True</button>
+            <button class="btn btn-danger" type="button" value="0" onclick="event.preventDefault(); document.getElementById('is_new').value = this.value;">False</button>
+        </div>
+        <span>
+            <p class="mt-1">set if product as new or not </p>
+        </span>
     </div>
-    <span>
-        <p class="mt-1">set if product as new or not </p>
-    </span>
-</div>
-<div class="col-sm-4">
-    <p class="mb-0">
-        <label for="stock_checking">Stock Checking</label>
-    </p>
-    <div class="btn-group w-25">
-        <button class="btn btn-success" type="button" value="1"
-            onclick="event.preventDefault(); document.getElementById('stock_checking').value = this.value;">Yes</button>
-        <button class="btn btn-danger" type="button" value="0"
-            onclick="event.preventDefault(); document.getElementById('stock_checking').value = this.value;">No</button>
+    <div class="col-sm-4">
+        <p class="mb-0">
+            <label for="stock_checking">Stock Checking</label>
+        </p>
+        <div class="btn-group w-25">
+            <button class="btn btn-success" type="button" value="1" onclick="event.preventDefault(); document.getElementById('stock_checking').value = this.value;">Yes</button>
+            <button class="btn btn-danger" type="button" value="0" onclick="event.preventDefault(); document.getElementById('stock_checking').value = this.value;">No</button>
+        </div>
+        <span>
+            <p class="mt-1">toggle stock checking to yes or no, product will not be tracked in stocks</p>
+        </span>
     </div>
-    <span>
-        <p class="mt-1">toggle stock checking to yes or no, product will not be tracked in stocks</p>
-    </span>
-</div>
 </div>
